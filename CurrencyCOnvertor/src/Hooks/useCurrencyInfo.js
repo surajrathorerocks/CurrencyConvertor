63ef241ae3f63dbfdca06316955eae8181fad94d
Suraj <131504529+surajrathorerocks@users.noreply.github.com>
2025-07-29 11:04:33 +0000

    buggy

---
+++ CurrencyCOnvertor/src/Hooks/useCurrencyInfo.js
@@ -0,0 +1,29 @@
+import { useEffect,useState } from "react";
+ function useCurrencyInfo(currency){
+    const [data, setdata] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((resp)=>resp.json())
+        .then((resp)=>setdata(resp[currency]))
+        console.log(data)
+    }, [currency])
+    console.log(data);
+    return data;
+    
+ }
+ export default useCurrencyInfo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
